9274f0d3bf9e7dbc95eb1978252a396aa467cae9
desert71 <amkazennov99@gmail.com>
2024-04-09 18:39:26 +0300

    Add main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, APIRouter
 import uvicorn
 from sqlalchemy import Column, Boolean, String
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
@@ -6,13 +6,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.dialects.postgresql import UUID
 import settings
 import uuid
+import re
+from pydantic import BaseModel, EmailStr, validator
 
 ##############################################
 # BLOCK FOR COMMON INTERACTION WITH DATABASE #
 ##############################################
 
 
-# create async engin for interaction with database
+# create async engine for interaction with database
 engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
 
 # create session for the interaction with database
@@ -57,3 +59,89 @@ class UserDAL:
         self.db_session.add(new_user)
         await self.db_session.flush()
         return new_user
+    
+
+#########################
+# BLOCK WITH API MODELS #
+#########################
+
+
+LETTER_MATCH_PATTERN = re.compile(r"^[а-яФ-Яa-zA-Z\-]+$")
+
+class TunedModel(BaseModel):
+    class Config:
+        """tells pydantic to convert even non dict obj to json"""
+
+        orm_mode = True
+
+class ShowUser(TunedModel):
+    user_id: uuid.UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
+class UserCreate(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+
+    @validator("name")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Name should contains only letters"
+            )
+        return value
+    
+    @validator("surname")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Surname should contains only letters"
+            )
+        return value
+    
+
+#########################
+# BLOCK WITH API ROUTES #
+#########################
+
+# create instance of the app
+app = FastAPI(title="BookServes")
+
+user_router = APIRouter()
+
+
+async def _create_new_user(body: UserCreate) -> ShowUser:
+    async with async_session() as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(
+                name=body.name,
+                surname=body.surname,
+                email=body.email,
+            )
+            return ShowUser(
+                user_id=user.user_id,
+                name=user.name,
+                surname=user.surname,
+                email=user.email,
+                is_active=user.is_active,
+            )
+
+
+@user_router.post('/', response_model=ShowUser)
+async def create_user(body: UserCreate) -> ShowUser:
+    return await _create_new_user(body=body)
+
+# create instance for the routes
+main_api_router = APIRouter()
+
+#set routes to the app instance
+main_api_router.include_router(user_router, prefix='/user', tags=["user"])
+app.include_router(main_api_router)
+
+if __name__ == "__main__":
+    #run app on the host and port
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ env = Env()
 
 REAL_DATABASE_URL = env.str(
     "REAL_DATABASE_URL",
-    default="postgresql+asyncpg://USER1:123456@0.0.0.0:5432/DB1"
+    default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres"
 ) # connect string for the database
